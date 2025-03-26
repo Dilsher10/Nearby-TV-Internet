6ffa4af34b7b2e94ffb457ee66ba1c09fa64ee9c
dilsher10 <dilsher.dahri10@gmail.com>
2025-03-27 02:36:15 +0500

    changes

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -10,27 +10,54 @@ const page = () => {
             <Navbar />
 
             <section className='policyHeader mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8'>
-                <h1 className='lg:py-10'>PRIVACY POLICY</h1>
+                <h1 className='lg:py-10'>Privacy Policy for NearbyTVInternet</h1>
             </section>
 
             <section className='policyContent mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 pb-10'>
-                <p>Scope of this policy:</p>
+                <p className='font-bold'>International Use and Consent</p>
 
-                <p>This Privacy Policy applies to nearbytvinternet.com operation of this Web site under express agreement with Charter Spectrum ™ and nearbytvinternet.com method of collection and use of personal information gathered by us for Charter Spectrum ™. However, the privacy practices of our clients and media providers are not covered by this Privacy Policy.</p>
+                <p>If you access our site from outside the United States, NearbyTVInternet may handle your personal data according to privacy laws that may differ from those in your country. By using our website internationally, you consent to the transfer of your information to jurisdictions outside your home country. Additionally, by engaging with our website, you agree that we may use your personal information to communicate electronically or otherwise regarding your website usage, product updates, and services. To opt out of promotional communications, please follow the unsubscribe instructions included in our messages.</p>
 
-                <p>This Privacy Policy applies to your use of nearbytvinternet.com. If you leave this site via a link or otherwise, this Privacy Policy does not apply, and nearbytvinternet.com cannot be and is not responsible for how personal information collected from you on other Web sites is used by the operators of such Web sites. Please be sure and check those Web sites’ Privacy Policies to ensure you are aware of their practices.</p>
+                <p className='font-bold'>Data Collection and Usage</p>
 
-                <p>nearbytvinternet.com may set and access our own cookies on your computer to allow you to buy and interact more conveniently and effectively with our Web sites. We also use web beacons to gather anonymous, aggregated statistical information (e.g. unique hits, page views) about our Web site traffic. This information is used to optimize your online experience by simplifying the process by which you order or interact with our Web sites.</p>
+                <p>At NearbyTVInternet, we prioritize your privacy and do not collect sensitive information without your explicit consent. When you register as a customer, we collect necessary details such as your name, address, phone number, email address, and demographic data. We also monitor website activity, including pages viewed, visit frequency, and time spent on specific pages, to enhance user experience.</p>
 
-                <p>nearbytvinternet.com may share, as necessary, your personal information collected by us with third parties to help authenticate your order (i.e. make sure it’s really you) or to process your order (e.g. with shipping companies, with credit card processors, etc.). Additionally, we may use or share with our business partners this personal information for statistical and demographic analysis to help us and our business partners to ensure that you are receiving all of the products, services and discounts for which you are qualified and to provide you with better services and offerings.</p>
+                <p>Your email and contact information are used solely for communication between you and us. We track IP addresses to prevent fraudulent activity. Payment information is shared only with our payment processor and your financial institution to process transactions securely.</p>
 
-                <p>nearbytvinternet.com will not share, rent, lease, or sell your personal information (e.g. your name, your address, your email address, your credit card information) to any third party for their independent use or benefit without first obtaining your permission.</p>
+                <p className='font-bold'>Cookies and Log Data</p>
 
-                <p>nearbytvinternet.com may contact you by telephone, email or regular mail using the contact information you provide to nearbytvinternet.com for the purposes of communicating to you regarding your order, customer surveys and special offers. By providing such contact information to nearbytvinternet.com, you are authorizing nearbytvinternet.com to contact you in this manner. You may, however, request at any time you are contacted by telephone, email or regular mail that you no longer be contacted by</p>
+                <p>Our website may use cookies to provide a tailored experience. Cookies are small text files stored on your computer that save registration details and other preferences to simplify future visits. While cookies enhance your browsing experience, most browsers allow you to disable them. However, this may limit certain site functionalities. You may also delete cookies from your device after using our site.</p>
 
-                <p>When you order products or services from us or register with this Site, we may collect your: Name Mailing and billing address E-mail address Phone number Credit card number and expiration date Social Security Number</p>
+                <p>Additionally, we may collect anonymized data about operating systems and browser types to improve website performance and user experience.</p>
 
-                <p>For Questions or Concerns about This Privacy Policy: email us at: <Link href={"mailto:info@nearbytvinternet.com"}>info@nearbytvinternet.com</Link> : Privacy Policy Coordinator.</p>
+                <p className='font-bold'>Agreement to Terms</p>
+
+                <p>By using NearbyTVInternet, you agree to this Privacy Policy. If you disagree with any part of it, please refrain from using the site. This policy may be updated periodically, and any changes will take effect upon posting. Continued use of the site after changes signifies acceptance of the updated terms.</p>
+
+                <p className='font-bold'>Policy Updates</p>
+
+                <p>We reserve the right to modify this Privacy Policy to reflect changes in our practices. Updates will be posted on this page or communicated via email. We encourage you to review the policy periodically to stay informed about updates.</p>
+
+                <p className='font-bold'>Security Measures</p>
+
+                <p>We implement recognized security protocols to protect your personal information from unauthorized access, misuse, alteration, or loss. Access to your data is restricted to authorized personnel who adhere to strict confidentiality standards.</p>
+
+                <p className='font-bold'>Children’s Privacy</p>
+
+                <p>NearbyTVInternet is not intended for individuals under the age of 13, and we do not knowingly collect personal information from children. We are committed to safeguarding children’s privacy and complying with applicable regulations.</p>
+
+                <p className='font-bold'>Limitations and Disclosures</p>
+
+                <p>While we strive to protect your privacy, we cannot guarantee absolute security due to potential legal or technological limitations. When required, we may disclose your information to private entities, law enforcement, or government agencies to address inquiries or resolve issues.</p>
+
+                <p className='font-bold'>Dispute Resolution</p>
+
+                <p>If you have concerns regarding our services or privacy practices, please contact us via email with detailed information about your complaint. We will acknowledge receipt within five business days and assign a representative to address your concerns. Updates to this privacy policy take effect immediately upon posting. Continued use of the website constitutes acceptance of the revised terms.</p>
+
+                <p className='font-bold'>Contact Information</p>
+
+                <p>For questions or concerns about this Privacy Policy, please contact us at <Link href={"mailto:info@nearbytvinternet.com"}>info@nearbytvinternet.com</Link> or via our contact form. By using our services, you agree to the terms and conditions outlined in this Privacy Policy.</p>
+
             </section>
 
             <Footer />
